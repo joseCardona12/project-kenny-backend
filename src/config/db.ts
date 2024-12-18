c911b93b9bd7d13a2c083a466dc9cb2477d16597
joseCardona12 <josesiprozmater@gmail.com>
2024-12-18 12:42:41 -0500

    feat: Add config sequeliz

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,18 @@
 import {Sequelize} from "sequelize-typescript";
+import {
+    db_host,
+    db_port,
+    db_name,
+    db_password,
+    db_username
+} from "./loadEnv";
 
 export const sequelize:Sequelize = new Sequelize({
     dialect: "mysql",
-
+    host: db_host!,
+    port: parseInt(db_port),
+    username: db_username,
+    password: db_password,
+    database:db_name,
+    models: [],
 })
--- src/config/loadEnv.ts
+++ src/config/loadEnv.ts
@@ -2,4 +2,9 @@ process.loadEnvFile();
 
 export const {
     PORT:port = "3002",
+    DB_NAME: db_name = "project-kenny",
+    DB_HOST: db_host = "localhost",
+    DB_PORT:db_port = "3200",
+    DB_USER: db_username = "root",
+    DB_PASSWORD: db_password = "root",
 } = process.env;
